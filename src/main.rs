1bad43adc030f61c953fbb8ff152b51e82202f3c
Debjit <github@debjit.dev>
2025-08-14 16:37:35 +0530

    correct receipts

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,40 @@ use gnosis_primitives::header::GnosisHeader;
 use reth_era::{
     e2s_types::E2sError,
     era1_file::Era1Reader,
-    execution_types::{BlockTuple, DecodeCompressed},
+    execution_types::BlockTuple,
+    DecodeCompressed
 };
 use reth_primitives::TransactionSigned;
 use std::{error::Error, fs::File, path::Path};
+use reth_ethereum_primitives::Receipt;
 
 mod rpc_log;
+
+type ReceiptType = ReceiptWithBloom<Receipt>;
 
 fn decode_block<E>(
     block: Result<BlockTuple, E>,
 ) -> eyre::Result<(
     GnosisHeader,
     BlockBody<TransactionSigned, GnosisHeader>,
-    Vec<ReceiptWithBloom>,
+    Vec<ReceiptType>,
 )>
 where
     E: From<E2sError> + Error + Send + Sync + 'static,
 {
     let block = block?;
 
-    let header = block.header.decode()?;
+    let header: GnosisHeader = block.header.decode()?;
     let body = block.body.decode()?;
-    let receipts = block.receipts.decode()?;
+    // println!("Receipts: {}", block.receipts.data.len());
+    let receipts: Vec<ReceiptType> = block.receipts.decode().unwrap_or_else(|_| {
+        // If decoding fails, we can return an empty vector or handle the error as needed
+        println!("Failed to decode receipts, returning empty vector");
+        vec![]
+    });
+    // let receipts: Vec<ReceiptWithBloom> = vec![];
+
+    println!("Height: {}", header.number);
 
     Ok((header, body, receipts))
 }
@@ -40,11 +52,32 @@ fn get_topic(log: &Log, index: usize) -> Vec<u8> {
 }
 
 fn main() -> Result<()> {
-    let file = File::open(Path::new("gnosis-00000-ac7f28ba.era1"))?;
+    let era_num = "00000";
+
+    let era_startswith = format!("gnosis-{}", era_num);
+    let era_directory = "/Users/deb/Gnosis/gnosis_downloader/downloads";
+    // find all files in the era directory that start with the era_startswith, ideally this would return just one file
+    let era_files: Vec<_> = std::fs::read_dir(era_directory)?
+        .filter_map(Result::ok)
+        .filter(|entry| entry.file_name().to_str().map_or(false, |s| s.starts_with(&era_startswith)))
+        .collect();
+
+    let file = File::open(era_files.get(0).map(|e| e.path()).unwrap_or_else(|| {
+        panic!("No era file found starting with the specified prefix")
+    }))?;
+
+    // let file = File::open(Path::new("gnosis-04800-9d927140.era1"))?;
     let reader = Era1Reader::new(file);
 
+    let mut i = 0;
+
     for block in reader.iter().map(decode_block) {
+        println!("Block: {}", i);
         let (header, body, receipts) = block?;
+        println!("Txs: {}, Receipts: {}", body.transactions.len(), receipts.len());
+        if body.transactions.len() != receipts.len() {
+            panic!("Warning: Mismatch between number of transactions and receipts for block {}", header.number);
+        }
         let block_hash = header.hash_slow();
 
         for (transaction_index, receipt) in receipts.iter().enumerate() {
@@ -65,9 +98,14 @@ fn main() -> Result<()> {
                     log_index: log_index as u64,
                 };
 
-                println!("{}", rpc_log);
+                // println!("{}", rpc_log);
             }
         }
+        i += 1;
+        if i > 50 {
+            break;
+        }
+        // break;
     }
 
     Ok(())
